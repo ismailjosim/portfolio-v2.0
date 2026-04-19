0725b39f5d31f8e9a42601a7099fe7dee397c9e3
ismailjosim <ismailjosim99@gmail.com>
2026-04-19 16:53:55 +0600

    feat: modify skill table, add project column and project filter for showing dashboard project

---
+++ src/components/modules/projectsManagement/ProjectFilter.tsx
@@ -0,0 +1,15 @@
+import ClearFiltersButton from '../../shared/ClearFiltersButton'
+import RefreshButton from '../../shared/RefreshButton'
+import SearchFilter from '../../shared/SearchFilter'
+
+const ProjectFilter = () => {
+	return (
+		<div className='space-y-3 flex justify-end gap-4'>
+			<SearchFilter paramName='searchTerm' placeholder='Search blogs...' />
+			<ClearFiltersButton />
+			<RefreshButton />
+		</div>
+	)
+}
+
+export default ProjectFilter
