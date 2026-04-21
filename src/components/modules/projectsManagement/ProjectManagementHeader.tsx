e9f255548a2ddc7232746fdcb2e11733e60b1567
ismailjosim <ismailjosim99@gmail.com>
2026-04-21 16:57:25 +0600

    feat: add project form dialog component, project management header component and project interface for project

---
+++ src/components/modules/projectsManagement/ProjectManagementHeader.tsx
@@ -0,0 +1,53 @@
+'use client'
+import { Plus } from 'lucide-react'
+import ManagementPageHeader from '../../shared/ManagementPageHeader'
+import AddBlogModal from './ProjectFormDialog'
+import { useRouter } from 'next/navigation'
+import { useState, useTransition } from 'react'
+
+const ProjectManagementHeader = () => {
+	const router = useRouter()
+	const [, startTransition] = useTransition()
+	const [isDialogOpen, setIsDialogOpen] = useState(false)
+
+	//force remount to reset state of form
+	const [dialogKey, setDialogKey] = useState(0)
+
+	const handleOpenDialog = () => {
+		setDialogKey((prev) => prev + 1)
+		setIsDialogOpen(true)
+	}
+
+	const handleCloseDialog = () => {
+		setIsDialogOpen(false)
+	}
+
+	const handleSuccess = () => {
+		startTransition(() => {
+			router.refresh()
+		})
+	}
+
+	return (
+		<>
+			<AddBlogModal
+				key={dialogKey}
+				open={isDialogOpen}
+				onClose={handleCloseDialog}
+				onSuccess={handleSuccess}
+			/>
+
+			<ManagementPageHeader
+				title='All Blogs'
+				description='Manage All Blogs'
+				action={{
+					label: 'Add Blog',
+					icon: Plus,
+					onClick: handleOpenDialog,
+				}}
+			/>
+		</>
+	)
+}
+
+export default ProjectManagementHeader
